880cc881fe833e119e45eaf863f9a05e7208428d
Eli Merel <elimerel17@gmail.com>
2026-04-19 23:20:40 -0400

    Delete RealVerdictFolder directory

--- RealVerdictFolder/serve.py
+++
@@ -1,8 +0,0 @@
-import os, http.server, socketserver
-
-os.chdir('/Users/elishamerel/Documents/RealVerdictFolder')
-PORT = 5173
-Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
-    httpd.serve_forever()
--- RealVerdictFolder/serve.rb
+++
@@ -1,7 +0,0 @@
-require 'webrick'
-server = WEBrick::HTTPServer.new(
-  Port: 5173,
-  DocumentRoot: '/Users/elishamerel/Documents/RealVerdictFolder'
-)
-trap('INT') { server.shutdown }
-server.start
